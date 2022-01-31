1bf4e062fce765f4987027afda7602a58c4a754c
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2022-02-01 00:08:20 +0100

    m

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.public')
 
 @section('styles')
-<link rel="stylesheet" href="style.css">
+<!-- public\js\features\typewriter.js -->
+<link rel="stylesheet" href="{{ asset('css\pages\index.css?v=1a040064279b') }}">
+
+
+<link rel="stylesheet" href="{{ asset('css/pages/home.css?v=1a040064279b') }}">
 @endsection
 
 
@@ -10,35 +14,42 @@ Home Page
 @endsection
 
 @section('content')
-<main>
+<main class="container" id="container menu_header">
+  <nav  id="menu">
+    <div><b class="myName">Vincent.</b></div>
+    <div class="" id="nav">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="/contact">Contact Me</a></li>
+        <li><a href="/store">Store</a></li>
+      </ul>
+    </div>
+  </nav>
   <!-- list of movies -->
-  <div class="container movies_home">
-  @foreach($movies as $movie)
-    <div id="learn" class="p-5">
-        <div class="container">
-          <div class="row align-items-center justify-content-between">
-            <div class="col-md">
-              <img src="{{$movie->poster}}" class="img-fluid" alt="" />
-            </div>
-            <div class="col-md p-5">
-              <h2>{{$movie->name}}</h2>
-              <p class="lead">
-                {{$movie->descript}}
-              </p>
-              <div>
-               <span><b>Location:</b> </span> <span>Ikeja</span>
-              </div>
-              <div>
-               <span><b>Show Time:</b> </span> <span>{{$movie->showtime}}</span>
-              </div>
-              <a href="#" class="btn btn-light mt-3">
-                <i class="bi bi-chevron-right"></i>Get Ticket Now
-              </a>
-            </div>
-          </div>
-        </div>
+  <style>
+    .btn{
+      color: white;
+      font-weight: bold;
+    }
+    .button{
+      background-color: #122AFF;
+      border-radius: 5px;
+    }
+  </style>
+  <div id="hero_section">
+    <div id="hero_left">
+      <h2 class="hero_title">I Build Digital Products and Business Solutions</h2>
+      <div class="btn_container">
+        <button class="btn button">Leave Message</button>
+        <a style="color: white;" class="btn" href="">Portfolio</a>
       </div>
     </div>
-  @endforeach
+    <div id="hero_right">
+      <!-- <img src="{{asset('siteImg/Rectangle 9565bg.jpg')}}" alt=""> -->
+      <!-- <img src="https://media.istockphoto.com/photos/suspicious-young-man-looking-at-camera-through-magnifier-picture-id1198224243" alt=""> -->
+    </div>
+  </div>
 </main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,6 @@ use App\Http\Controllers\PageController;
 // });
 
 Route::get('/', [PageController::class, 'home']);
-Route::get('/dashboard/movies/new', [MoviesController::class, 'create'])->middleware(['auth'])->name('new_movie');
-Route::post('/dashboard/movies/store', [MoviesController::class, 'store'])->middleware(['auth']);
-Route::get('/dashboard/location/new', [LocationController::class, 'create'])->middleware(['auth'])->name('new_location');
-Route::post('/dashboard/location/store', [LocationController::class, 'store'])->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
